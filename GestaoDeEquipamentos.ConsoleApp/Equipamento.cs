1011cc3b50fe9ae08140a7821e4e72497dd4ecb2
brunsssantos <brunasan7os@gmail.com>
2025-05-08 19:15:56 -0300

    Realizada a refatoração do repositório

---
+++ GestaoDeEquipamentos.ConsoleApp/Equipamento.cs
@@ -0,0 +1,14 @@
+﻿namespace GestaoDeEquipamentos.ConsoleApp;
+
+// Regras de negócio
+public class Equipamento
+{
+    public int id;
+    public string nome;
+    public decimal precoAquisicao;
+    public string numeroSerie;
+    public string fabricante;
+    public DateTime dataFabricacao;
+
+}
+
