501c573252556a4b7dcba72245fb65aa744221b4
brunsssantos <brunasan7os@gmail.com>
2025-08-11 20:47:26 -0300

    Implementado HomeController e ASP.NET MVC

---
+++ GestaoDeEquipamentos.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoDeEquipamentos.WebApp.Controllers;
+
+public class HomeController : Controller
+{
+    // Ação que retorna a View padrão do HomeController
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
