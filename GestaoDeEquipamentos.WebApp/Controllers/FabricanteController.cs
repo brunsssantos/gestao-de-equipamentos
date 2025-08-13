493fc56c98b38e93b729c878a912cfba8af684ff
brunsssantos <brunasan7os@gmail.com>
2025-08-13 13:14:16 -0300

    Implementados ViewModels de Fabricante

--- GestaoDeEquipamentos.WebApp/Controllers/FabricanteController.cs
+++ GestaoDeEquipamentos.WebApp/Controllers/FabricanteController.cs
@@ -1,6 +1,7 @@
 ﻿using GestaoDeEquipamentos.Dominio.ModuloFabricante;
 using GestaoDeEquipamentos.Infraestrutura.Arquivos.Compartilhado;
 using GestaoDeEquipamentos.Infraestrutura.Arquivos.ModuloFabricante;
+using GestaoDeEquipamentos.WebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GestaoDeEquipamentos.WebApp.Controllers;
@@ -20,19 +21,27 @@ public class FabricanteController : Controller
     {
         List<Fabricante> fabricantes = repositorioFabricante.SelecionarRegistros();
 
+        VisualizarFabricantesViewModel visualizarVm = new VisualizarFabricantesViewModel(fabricantes);
+
         return View(fabricantes);
     }
 
     [HttpGet]
     public IActionResult Cadastrar()
     {
-        return View();
+        CadastrarFabricanteViewModel cadastrarVm = new CadastrarFabricanteViewModel();
+
+        return View(cadastrarVm);
     }
 
     [HttpPost]
-    public IActionResult Cadastrar(string nome, string email, string telefone)
+    public IActionResult Cadastrar(CadastrarFabricanteViewModel cadastrarVm)
     {
-        Fabricante novoFabricante = new Fabricante(nome, email, telefone);
+        Fabricante novoFabricante = new Fabricante(
+            cadastrarVm.Nome, 
+            cadastrarVm.Email, 
+            cadastrarVm.Telefone
+            );
 
         repositorioFabricante.CadastrarRegistro(novoFabricante);
 
@@ -47,13 +56,24 @@ public class FabricanteController : Controller
         if (fabricanteSelecionado == null)
             return RedirectToAction(nameof(Index));
 
-        return View(fabricanteSelecionado);
+        EditarFabricanteViewModel editarVm = new EditarFabricanteViewModel(
+            fabricanteSelecionado.Id,
+            fabricanteSelecionado.Nome,
+            fabricanteSelecionado.Email,
+            fabricanteSelecionado.Telefone
+            );
+
+        return View(editarVm);
     }
 
     [HttpPost]
-    public IActionResult Editar(int id, string nome, string email, string telefone)
+    public IActionResult Editar(int id, EditarFabricanteViewModel editarVm)
     {
-        Fabricante fabricanteEditado = new Fabricante(nome, email, telefone);
+        Fabricante fabricanteEditado = new Fabricante(
+            editarVm.Nome,
+            editarVm.Email,
+            editarVm.Telefone
+            );
 
         bool edicaoConcluida = repositorioFabricante.EditarRegistro(id, fabricanteEditado);
 
@@ -76,6 +96,9 @@ public class FabricanteController : Controller
 
         if (fabricanteSelecionado == null)
             return RedirectToAction(nameof(Index));
+
+        ExcluirFabricanteViewModel excluirVm = 
+            new ExcluirFabricanteViewModel(id, fabricanteSelecionado.Nome);
 
         return View(fabricanteSelecionado);
     }
