cb8d03aa09338c1986a8efc1732bd90f11f77f65
brunsssantos <brunasan7os@gmail.com>
2025-08-13 23:40:28 -0300

    Finalizada implementação do EquipamentoController

--- GestaoDeEquipamentos.WebApp/Models/EquipamentoViewModels.cs
+++ GestaoDeEquipamentos.WebApp/Models/EquipamentoViewModels.cs
@@ -1,5 +1,6 @@
 ﻿using GestaoDeEquipamentos.Dominio.ModuloEquipamento;
 using GestaoDeEquipamentos.Dominio.ModuloFabricante;
+using System.Runtime.CompilerServices;
 
 namespace GestaoDeEquipamentos.WebApp.Models;
 
@@ -29,6 +30,59 @@ public class CadastrarEquipamentoViewModel
     }
 }
 
+public class EditarEquipamentoViewModel
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public decimal PrecoAquisicao { get; set; }
+    public DateTime DataFabricacao { get; set; }
+    public int FabricanteId { get; set; }
+    public List<SelecionarFabricanteViewModel> FabricantesDisponiveis { get; set; }
+
+    public EditarEquipamentoViewModel()
+    {
+        FabricantesDisponiveis = new List<SelecionarFabricanteViewModel>();
+    }
+
+    public EditarEquipamentoViewModel(
+        int id,
+        string nome,
+        decimal precoAquisicao,
+        DateTime dataFabricacao,
+        int fabricanteId,
+        List<Fabricante> fabricantes
+        
+    ) : this() //quando tem construtor base
+    {
+
+        foreach (Fabricante f in fabricantes)
+        {
+            SelecionarFabricanteViewModel selecionarVm =
+                new SelecionarFabricanteViewModel(f.Id, f.Nome);
+
+            FabricantesDisponiveis.Add(selecionarVm);
+        }
+
+        Id = id;
+        Nome = nome;
+        PrecoAquisicao = precoAquisicao;
+        DataFabricacao = dataFabricacao;
+        FabricanteId = fabricanteId;
+    }
+}
+
+public class ExcluirEquipamentoViewModel
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+
+    public ExcluirEquipamentoViewModel(
+        int id,string nome) 
+    {
+        Id = id;
+        Nome = nome;
+    }
+}
 public class SelecionarFabricanteViewModel
 {
     public int Id { get; set; }
